d8bef550a410b703aa49a777ffd1d3ea6141057a
Franziska Schmidt <hello@franka.tech>
2019-12-02 22:20:15 +0100

    day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -37,6 +37,52 @@ fn day_1_2() {
     println!("Day 1 - 2: {}", sum);
 }
 
+fn read_input_day2() -> Vec<i32> {
+    let mut file = File::open("./inputs/day_2").expect("File could not be opened");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).expect("Can't read from file");
+
+    let numbers: Vec<i32> = contents.trim().split(",").map(|s| s.parse().expect("Could not parse s")).collect();
+    numbers
+}
+
+fn consume_line(nums: &Vec<i32>, index: &usize) -> i32 {
+    let operator = nums[*index];
+
+    match operator {
+        1 => {
+            let i1 = nums[index + 1] as usize;
+            let i2 = nums[index + 2] as usize;
+            nums[i1] + nums[i2]
+        }
+        2 => {
+            let i1 = nums[index + 1] as usize;
+            let i2 = nums[index + 2] as usize;
+            nums[i1] * nums[i2]
+        }
+        _ => panic!("Unknown operator: {}", operator)
+    }
+}
+
+fn day_2_1() {
+    let mut nums = read_input_day2();
+
+    nums[1] = 12;
+    nums[2] = 2;
+
+    let len = nums.len() - 1;
+
+    for i in 0..len {
+        if (i % 4 != 0) { continue; }
+        if nums[i] == 99 { break; }
+        let store_val = nums[i + 3];
+
+        nums[store_val as usize] = consume_line(&nums, &i);
+    };
+
+    println!("final answer: {:?}", nums[0]);
+}
+
 fn main() {
-    day_1_2();
+    day_2_1();
 }
