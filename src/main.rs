786a5e92014a93a971293755e782f787659aae10
Franziska Schmidt <hello@franka.tech>
2019-12-07 00:07:09 +0100

    part 4.2

--- src/main.rs
+++ src/main.rs
@@ -298,6 +298,31 @@ fn two_cons_nums(number: i32) -> bool {
     false
 }
 
+fn only_two_cons_nums(number: i32) -> bool {
+    let stri = number.to_string();
+    let (h, tail) = stri.split_at(1);
+    let mut head = h.chars().nth(0).unwrap();
+
+    for (i, n) in tail.chars().enumerate() {
+        if head == n {
+            let next = tail.chars().nth(i + 1);
+            let mut prev = None;
+            if i >= 1 { prev = stri.chars().nth(i - 1); }
+            println!("{}: {},{} : {:?}-{:?}-{:?}-{:?}", number, i, n, prev, head, n, next);
+
+            match (next, prev) {
+                (Some(next), Some(prev)) => { if next != n && prev != n { return true; } }
+                (Some(next), None) => { if next != n  { return true; }}
+                (None, Some(prev)) => { if prev != n  { return true; }}
+                (None, None) => { return true; }
+            }
+        }
+        head = n;
+    }
+    false
+}
+
+
 fn increasing_nums(number: i32) -> bool {
     let stri = number.to_string();
     let (h, tail) = stri.split_at(1);
@@ -315,14 +340,28 @@ fn day_4_1() {
     let mut passwords = vec![];
 
     for i in start..end {
-        if two_cons_nums(i as i32) && increasing_nums(i as i32) {
+        if only_two_cons_nums(i as i32) && increasing_nums(i as i32) {
             passwords.push(i as i32);
         }
     }
     println!("{:?}", passwords.len());
 }
 
+fn day_4_2() {
+    let start = 138241;
+    let end = 674034;
+    let mut passwords = vec![];
+
+    for i in start..end {
+        if increasing_nums(i as i32) && only_two_cons_nums(i as i32) {
+            passwords.push(i as i32);
+        }
+    }
+    println!("{:?}", passwords);
+    println!("{:?}", passwords.len());
+}
+
 
 fn main() {
-    day_4_1();
+    day_4_2();
 }
